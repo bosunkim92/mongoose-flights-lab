69f8c892e1d55b6351856fb48ab558eba54a7d02
BOSUN KIM <bosunkim@BOSUNs-iMac.local>
2021-06-16 00:52:13 -0700

    Mongoose lab part 2 progress

--- mongoose-flights-lab/controllers/flights.js
+++ mongoose-flights-lab/controllers/flights.js
@@ -1,12 +1,37 @@
-const Flight = require('./models/flight');
+const Flight = require('../models/flight');
 
 module.exports = {
     index,
-    new: newFlight
+    new: newFlight,
+    create,
+    show
 }
 
 function index(req, res) {
-    Flight.find({}, function(err, flight){
+    Flight.find({}, function(err, flights){
         res.render('flights/index', {title: 'All Flights', flights})
+    })
+}
+
+function newFlight(req, res) {
+    res.render('flights/new');
+}
+
+function create(req, res) {
+
+    if(req.body.departs === ''){
+        req.body.departs = Flight.departs;
+    }
+
+    Flight.create(req.body, function(err, flight){
+
+        res.redirect('/flights');        
+    })
+}
+
+function show(req, res){
+    console.log('HTTP request is hitting the right show controller function');
+    Flight.findById(req.params.id, function(err, flight){
+        res.render('flights/show');
     })
 }
--- mongoose-flights-lab/models/flight.js
+++ mongoose-flights-lab/models/flight.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+    },
+    arrival: {
+        type: Date
+    }
+}, {
+    timestamps: true
+})
+
 const flightSchema = new Schema({
     airline: {
         type: String,
@@ -9,7 +21,7 @@ const flightSchema = new Schema({
     airport: {
         type: String,
         enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'],
-        default: 'DEN'
+        default: 'DEN',
     },
     flightNo: {
         type: Number,
@@ -19,10 +31,21 @@ const flightSchema = new Schema({
     },
     departs: {
         type: Date,
-        //default: currentDateYear + 1
-    }
+        default: dateYearLater(),
+    },
+    destinations: [destinationSchema]
+}, {
+    timestamps: true
 });
 
-
+function dateYearLater() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    return new Date(year+1, month, day, hour, minute);
+}
 
 module.exports = mongoose.model('Flight', flightSchema)
